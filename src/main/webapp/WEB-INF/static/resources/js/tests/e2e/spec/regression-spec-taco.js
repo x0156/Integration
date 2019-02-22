317b68fe33aea94e2a5a6f3d8168335531bae4bb
swethacts <SwethaReddy.Baikadi@cognizant.com>
2019-02-22 14:50:51 +0530

    Update regression-spec-taco.js

--- src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/regression-spec-taco.js
+++ src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/regression-spec-taco.js
@@ -5,7 +5,7 @@ describe('Smoke Suite for the Community Application', function() {
 
 	beforeEach(function() {
 	  browser.ignoreSynchronization = true;
-	  browser.get('http://ec2-3-17-187-0.us-east-2.compute.amazonaws.com:3000/#/login '); 
+	  browser.get('http://ec2-3-17-187-0.us-east-2.compute.amazonaws.com:3000/#/login'); 
 	  browser.driver.manage().window().maximize();
 	  browser.driver.sleep(3000);   
 	});

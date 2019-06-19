af23d73d6f372ac9313c1986b50e26be9d8a22de
swethacts <SwethaReddy.Baikadi@cognizant.com>
2019-06-19 15:54:37 +0530

    Update regression-spec-angular.js

--- src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/regression-spec-angular.js
+++ src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/regression-spec-angular.js
@@ -1,4 +1,3 @@
-
 // Tests for the sample REST APP - http://10.120.101.126:3000/
 
 describe('Regression Suite for the Community Application', function() {
@@ -11,57 +10,67 @@ describe('Regression Suite for the Community Application', function() {
         });
 	
 	
-	function UserLoginToApp(ID,PASSWORD){
+	function LoginToApp(){
 		
 		var usernamelInput = element(by.id('username'));
-		var passwordInput = element(by.id('password'));http://192.168.40.159:3000/#/loginr OkButton = element(by.buttonText('OK'));			
+		var passwordInput = element(by.id('password'));
+		var OkButton = element(by.buttonText('OK'));			
 			usernamelInput.clear();
-			usernamelInput.sendKeys(ID);
+			usernamelInput.sendKeys('admin');
 			passwordInput.clear();
-			passwordInput.sendKeys(PASSWORD);
+			passwordInput.sendKeys('test123');
 			OkButton.click();
 			browser.driver.sleep(3000);
-			expect(browser.getTitle()).toEqual('REST EXAMPLE APP');				
-
-	}	
+			//expect(browser.getTitle()).toEqual('REST EXAMPLE APP');				
+			//var signOutLink = element(by.xpath("//a[text()='Logout']"));
+			//signOutLink.click();
+	}
 	
-  it('Testcase001 > Test the LOGIN Functionality', function() {
+  it('Test the SignUp Page with Elements', function() {
 	  
-	UserLoginToApp('admin','test123');
 	browser.driver.sleep(3000);
+	var signUpButton = element(by.css('.btn.btn-lg.btn-success.ng-binding'));	
+	signUpButton.click();
+	browser.driver.sleep(3000);
+		var signupUsernameInput = element(by.id('username'));
+		var sigupPasswordInput = element(by.id('password'));
+		var sigupEmailInput = element(by.id('email'));
+		var sigupNameInput = element(by.id('name'));
+		var OkButton = element(by.css('.btn.btn-primary.btn-lg.ng-binding'));
+		
+		expect(signupUsernameInput.isPresent()).toEqual(true);
+		expect(sigupPasswordInput.isPresent()).toEqual(true);
+		expect(sigupEmailInput.isPresent()).toEqual(true);
+		expect(sigupNameInput.isPresent()).toEqual(true);
+	console.log('The SignUp page loaded successfully');		
+  });
+	
+	
+  it('Test the LOGIN Functionality', function() {
+	  
+	LoginToApp();
 	var signOutLink = element(by.xpath("//a[text()='Logout']"));
 	signOutLink.click();
 	console.log('The User logged in successfully');	
 	
   });
   
-    it('Testcase002 > Test the SignUp Functionality', function() {
-	  
-	var signUpButton = element(by.css('.btn.btn-lg.btn-success.ng-binding'));
-	signUpButton.click();
-	browser.driver.sleep(3000);
-	var today = new Date();
-	var user = 'leapuser'+today.getHours()+today.getMilliseconds()+today.getMinutes();
-		var signupUsernameInput = element(by.name('username'));
-		var sigupPasswordInput = element(by.id('password'));
-		var sigupEmailInput = element(by.id('email'));
-		var sigupNameInput = element(by.id('name'));
-		var OkButton = element(by.css('.btn.btn-primary.btn-lg.ng-binding'));		
-		signupUsernameInput.sendKeys(user);
-		sigupPasswordInput.sendKeys('test1ng');
-		sigupEmailInput.sendKeys(user+'@leap.com');
-		sigupNameInput.sendKeys('LEAP Tester');
-		browser.driver.sleep(3000);
-		OkButton.click();
-	browser.driver.sleep(3000);	
-	console.log('The User ['+ user +'] registered successfully');	
-/* 	UserLoginToApp(user,'test1ng');
-	browser.driver.sleep(3000);	
-	console.log('The user login post sign up was successfully');
-	var signOutLink = element(by.xpath("//a[text()='Logout']"));
-	signOutLink.click(); */
+  it('Test the Add Post Functionality', function() {
 	
+	var addButton = element(by.css('.btn.btn-success.btn-block.ng-binding'));
+	var nameInput = element(by.name('title'));
+	var contentInput = element(by.name('content'));
+	var saveButton = element(by.css('.btn.btn-primary.btn-lg.ng-binding'));	
+	LoginToApp();
+	browser.driver.sleep(3000);
+	expect(addButton.isPresent()).toEqual(true);
+	addButton.click();
+	expect(nameInput.isPresent()).toEqual(true);
+	expect(contentInput.isPresent()).toEqual(true);
+	expect(saveButton.isPresent()).toEqual(true);
+	console.log('The Add Post page was available with the web elements successfully');
+	var signOutLink = element(by.xpath("//a[text()='Logout']"));
+	signOutLink.click();
   });
   
  
-});
